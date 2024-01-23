c48cca60eb95d535f9a124efddf6f31cd2d3c43d
Arshdeep Kaur <sainiarshdeep114@gmail.com>
2024-01-23 21:33:39 +0530

    Review Changes

--- models/review.js
+++ models/review.js
@@ -1,2 +1,19 @@
 const mongoose = require("mongoose");
-const Scheema = mongoose.Schema;
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  comment: String,
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Review = new mongoose.model("Review", reviewSchema);
+
+module.exports = Review;
