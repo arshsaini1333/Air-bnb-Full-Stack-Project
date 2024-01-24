30fa49fb44b027c355442e49e229935ec0ba3001
Arshdeep Kaur <sainiarshdeep114@gmail.com>
2024-01-24 12:08:25 +0530

    Review

--- models/listing.js
+++ models/listing.js
@@ -28,6 +28,10 @@ const listingScheema = new Scheema({
   country: {
     type: String,
   },
+  review: {
+    type: Scheema.Types.ObjectId,
+    ref: "Review",
+  },
 });
 
 const Listing = mongoose.model("Listing", listingScheema);
