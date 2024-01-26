6b52f85afc63e40389ad19cefd15c6fb25e13c01
Arshdeep Kaur <sainiarshdeep114@gmail.com>
2024-01-26 17:03:22 +0530

    Setting Passports

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const review = require("./routes/review.js");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 //Defining port and Listning request
 const port = 8080;
@@ -66,12 +69,30 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash()); //Flash
 
-//Middleware
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+//TO serialize and deserialoze users into sessions
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Flash Middleware
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
 
   next();
+});
+
+app.get("/demouser", async (req, res) => {
+  let fakeUser = new User({
+    email: "fakeUser@gmail.com",
+    username: "fakeUser",
+  });
+  let reg = await User.register(fakeUser, "password");
+  res.send(reg);
 });
 
 //Using Our Routes
