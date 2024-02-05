ed955346f2352785e5027612f2f1a84a68e964a8
Arshdeep Kaur <sainiarshdeep114@gmail.com>
2024-02-05 18:02:25 +0530

    Route.route

--- routes/listing.js
+++ routes/listing.js
@@ -3,51 +3,29 @@ const route = express.Router();
 const wrapAsync = require("../utils/wrapAsync");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-
 const listingRoutes = require("../controllers/listing.js");
-//Index Route
 
-route.get("/", wrapAsync(listingRoutes.index));
+//Routes
 
-//Show Route
+route
+  .route("/")
+  .get(wrapAsync(listingRoutes.index))
+  .post(isLoggedIn, validateListing, wrapAsync(listingRoutes.saveNewListing));
+
+route
+  .route("/:id")
+  .put(
+    isOwner,
+    validateListing,
+    isLoggedIn,
+    wrapAsync(listingRoutes.saveUpdatedList)
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingRoutes.deleteListing));
 
 route.get("/:id/show", wrapAsync(listingRoutes.show));
 
-//--------------NEW--------------------
-
-//Geting Rquest For new Listing
 route.get("/new", isLoggedIn, listingRoutes.newForm);
 
-//Getting Data For New Lsiting
-route.post(
-  "/",
-  isLoggedIn,
-  validateListing,
-  wrapAsync(listingRoutes.saveNewListing)
-);
-
-//----------EDIT--------------
-
-//Editing our List
-
 route.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingRoutes.editForm));
-
-//Adding updated data to the DB
-route.put(
-  "/:id",
-  isOwner,
-  validateListing,
-  isLoggedIn,
-  wrapAsync(listingRoutes.saveUpdatedList)
-);
-
-//Deleting list
-
-route.delete(
-  "/:id",
-  isLoggedIn,
-  isOwner,
-  wrapAsync(listingRoutes.deleteListing)
-);
 
 module.exports = route;
